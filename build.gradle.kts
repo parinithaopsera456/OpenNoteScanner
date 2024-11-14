496bc24b211596b0c42b8049b4101b416dc5adba
parinithaopsera456 <gparinitha@opsera.io>
2024-11-14 21:37:48 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,22 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
-        mavenCentral()
-        jcenter()
         google()
+        mavenCentral()
         maven(url = "https://jitpack.io")
+        maven { url = uri("https://jcenter.bintray.com/") }
     }
-
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    }
+}
 
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven { url = uri("https://jcenter.bintray.com/") }
     }
 }
 
