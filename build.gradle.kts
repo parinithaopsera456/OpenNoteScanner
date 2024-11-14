15b0d0234e420098b7c3b1aaff45fa25e1706a4b
parinithaopsera456 <gparinitha@opsera.io>
2024-11-14 22:33:33 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 buildscript {
     repositories {
         google()
-        mavenCentral()
-        maven(url = "https://jitpack.io")
-        maven { url = uri("https://jcenter.bintray.com/") }
+        mavenCentral() // Maven Central is important for a lot of popular libraries
+        maven(url = "https://jitpack.io") // JitPack for libraries hosted on GitHub
+        maven { url = uri("https://jcenter.bintray.com/") } // Deprecated, but sometimes required
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
@@ -15,9 +15,14 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://jitpack.io")
-        maven { url = uri("https://jcenter.bintray.com/") }
+        maven(url = "https://jitpack.io") // Make sure JitPack is included here
+        maven { url = uri("https://jcenter.bintray.com/") } // Include JCenter, even though it's deprecated
     }
+}
+
+dependencies {
+    // Try adding the missing dependency directly (if it's available in JitPack or Maven Central)
+    implementation("us.feras.mdv:markdownview:1.1.0")
 }
 
 tasks.register("clean", Delete::class) {
